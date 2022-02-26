de61b8cfcddec1c30bd7297e053bf294d5728590
Ashvith <9512421+Ashvith@users.noreply.github.com>
2022-02-26 13:11:10 +0530

    Replace Bootstrap with Bulma

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,7 @@
 class TodoList < ApplicationRecord
   validates :title, presence: true
+
+  after_create_commit -> { broadcast_prepend_later_to "todo_lists" }
+  after_update_commit -> { broadcast_update_later_to "todo_lists" }
+  after_destroy_commit -> { broadcast_remove_later_to "todo_lists" }
 end
